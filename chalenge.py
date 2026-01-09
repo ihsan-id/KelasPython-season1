0329f66c56bb979f14a19a65754c719e8e12caba
ihsan-id <aimedia023@gmail.com>
2026-01-09 10:58:06 +0700

    Add python files

---
+++ chalenge.py
@@ -0,0 +1,11 @@
+# Poin kesehatan awal
+health = 100
+
+# Minum ramuan (+25)
+health += 25
+
+# Diserang monster (-40)
+health -= 40
+
+# Tampilkan hasil akhir
+print("Poin kesehatan akhir pemain:", health)
---
+++ main.py
@@ -0,0 +1,4 @@
+print("my name is Ihsan")
+print("i am 22 years old")
+print("i am from indonesia")
+print("i am happy")
